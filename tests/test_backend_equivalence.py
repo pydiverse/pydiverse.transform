4dfe3b3be73ab4fb8a529082ba9e71270cab7c7d
Nicolas Camenisch <garnele007@gmail.com>
2022-06-24 12:53:41 +0200

    Implement many more test cases for backend_equivalence tests

--- tests/test_backend_equivalence.py
+++ tests/test_backend_equivalence.py
@@ -7,6 +7,7 @@ import pytest
 import sqlalchemy
 from pandas.testing import assert_frame_equal
 
+import pdtransform.core.dispatchers
 from pdtransform import λ
 from pdtransform.core.table import Table
 from pdtransform.core.verbs import alias, arrange, collect, filter, group_by, join, mutate, select, summarise, ungroup
@@ -87,15 +88,38 @@ def tables(names: list[str]):
     return pytest.mark.parametrize(param_names, params)
 
 
-def assert_result_equal(x, y, pipe_factory):
+def assert_result_equal(x, y, pipe_factory, exception = None, **kwargs):
     if not isinstance(x, (list, tuple)):
         x = (x,)
         y = (y,)
 
-    dfx = pipe_factory(*x) >> collect()
-    dfy = pipe_factory(*y) >> collect()
+    if exception:
+        with pytest.raises(exception):
+            pipe_factory(*x) >> collect()
+        with pytest.raises(exception):
+            pipe_factory(*y) >> collect()
+        return
+    else:
+        dfx = (pipe_factory(*x) >> collect()).reset_index(drop = True)
+        dfy = (pipe_factory(*y) >> collect()).reset_index(drop = True)
 
-    assert_frame_equal(dfx, dfy)
+    try:
+        assert_frame_equal(dfx, dfy, **kwargs)
+    except Exception as e:
+        print('First dataframe:')
+        print(dfx)
+        print('Second dataframe:')
+        print(dfy)
+        raise e
+
+
+@pdtransform.core.dispatchers.verb
+def full_sort(t: Table):
+    """
+    Ordering after join is not determined.
+    This helper applies a deterministic ordering to a table.
+    """
+    return t >> arrange(*t)
 
 
 class TestSelect:
@@ -105,13 +129,250 @@ class TestSelect:
         assert_result_equal(df1_x, df1_y, lambda t: t >> select(t.col1))
         assert_result_equal(df1_x, df1_y, lambda t: t >> select(t.col2))
 
+    @tables(['df1'])
+    def test_reorder(self, df1_x, df1_y):
+        assert_result_equal(df1_x, df1_y, lambda t: t >> select(t.col2, t.col1))
+
 
 class TestMutate:
+
+    @tables(['df2'])
+    def test_noop(self, df2_x, df2_y):
+        assert_result_equal(df2_x, df2_y, lambda t: t >> mutate(col1 = t.col1, col2 = t.col2, col3 = t.col3))
 
     @tables(['df1'])
     def test_multiply(self, df1_x, df1_y):
         assert_result_equal(df1_x, df1_y, lambda t: t >> mutate(x2 = t.col1 * 2))
         assert_result_equal(df1_x, df1_y, lambda t: t >> select() >> mutate(x2 = t.col1 * 2))
+
+    @tables(['df2'])
+    def test_reorder(self, df2_x, df2_y):
+        assert_result_equal(
+            df2_x, df2_y,
+            lambda t:
+            t >> mutate(col1 = t.col2, col2 = t.col1)
+        )
+
+        assert_result_equal(
+            df2_x, df2_y,
+            lambda t:
+            t >> mutate(col1 = t.col2, col2 = t.col1) >> mutate(col1 = t.col2, col2 = λ.col3, col3 = λ.col2)
+        )
+
+
+class TestJoin:
+
+    # TODO: Implement test for outer join. This doesn't work with sqlite
+    @tables(['df1', 'df2'])
+    @pytest.mark.parametrize('how', ['inner', 'left'])
+    def test_join(self, df1_x, df1_y, df2_x, df2_y, how):
+        assert_result_equal(
+            (df1_x, df2_x), (df1_y, df2_y),
+            lambda t, u:
+            t >> join(u, t.col1 == u.col1, how=how) >> full_sort()
+        )
+
+        assert_result_equal(
+            (df1_x, df2_x), (df1_y, df2_y),
+            lambda t, u:
+            t >> join(u, (t.col1 == u.col1) & (t.col1 == u.col2), how=how) >> full_sort()
+        )
+
+    # TODO: Implement test for outer join. This doesn't work with sqlite
+    @tables(['df1', 'df2'])
+    @pytest.mark.parametrize('how', ['inner', 'left'])
+    def test_join_and_select(self, df1_x, df1_y, df2_x, df2_y, how):
+        assert_result_equal(
+            (df1_x, df2_x), (df1_y, df2_y),
+            lambda t, u:
+            t >> select() >> join(u, t.col1 == u.col1, how=how) >> full_sort()
+        )
+
+        assert_result_equal(
+            (df1_x, df2_x), (df1_y, df2_y),
+            lambda t, u:
+            t >> join(u >> select(), (t.col1 == u.col1) & (t.col1 == u.col2), how=how) >> full_sort()
+        )
+
+    # TODO: Implement test for outer join. This doesn't work with sqlite
+    @tables(['df3'])
+    @pytest.mark.parametrize('how', ['inner', 'left'])
+    def test_self_join(self, df3_x, df3_y, how):
+        # Self join without alias should raise an exception
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t: t >> join(t, t.col1 == t.col1, how=how),
+            exception = ValueError
+        )
+
+        def self_join_1(t):
+            u = t >> alias('self_join')
+            return t >> join(u, t.col1 == u.col1, how=how) >> full_sort()
+        assert_result_equal(df3_x, df3_y, self_join_1)
+
+        def self_join_2(t):
+            u = t >> alias('self_join')
+            return t >> join(u, (t.col1 == u.col1) & (t.col2 == u.col2), how = how) >> full_sort()
+        assert_result_equal(df3_x, df3_y, self_join_2)
+
+        def self_join_3(t):
+            u = t >> alias('self_join')
+            return t >> join(u, (t.col2 == u.col3), how = how) >> full_sort()
+        assert_result_equal(df3_x, df3_y, self_join_3)
+
+
+class TestFilter:
+
+    @tables(['df2'])
+    def test_noop(self, df2_x, df2_y):
+        assert_result_equal(df2_x, df2_y, lambda t: t >> filter())
+        assert_result_equal(df2_x, df2_y, lambda t: t >> filter(t.col1 == t.col1))
+
+    @tables(['df2'])
+    def test_simple_filter(self, df2_x, df2_y):
+        assert_result_equal(df2_x, df2_y, lambda t: t >> filter(t.col1 == 2))
+        assert_result_equal(df2_x, df2_y, lambda t: t >> filter(t.col1 != 2))
+
+    @tables(['df2'])
+    def test_chained_filters(self, df2_x, df2_y):
+        assert_result_equal(
+            df2_x, df2_y,
+            lambda t:
+            t >> filter(1 < t.col1) >> filter(t.col1 < 5)
+        )
+
+        assert_result_equal(
+            df2_x, df2_y,
+            lambda t:
+            t >> filter(1 < t.col1) >> filter(t.col3 < 0.25)
+        )
+
+    @tables(['df3'])
+    def test_filter_empty_result(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> filter(t.col1 == 0) >> filter(t.col2 == 2) >> filter(t.col4 < 2),
+
+            check_dtype = False
+        )
+
+
+class TestArrange:
+
+    @tables(['df1'])
+    def test_noop(self, df1_x, df1_y):
+        assert_result_equal(df1_x, df1_y, lambda t: t >> arrange())
+
+    @tables(['df2'])
+    def test_arrange(self, df2_x, df2_y):
+        assert_result_equal(df2_x, df2_y, lambda t: t >> arrange(t.col1))
+        assert_result_equal(df2_x, df2_y, lambda t: t >> arrange(-t.col1))
+        assert_result_equal(df2_x, df2_y, lambda t: t >> arrange(t.col3))
+        assert_result_equal(df2_x, df2_y, lambda t: t >> arrange(-t.col3))
+
+    @tables(['df2'])
+    def test_arrange_null(self, df2_x, df2_y):
+        assert_result_equal(df2_x, df2_y, lambda t: t >> arrange(t.col2))
+        assert_result_equal(df2_x, df2_y, lambda t: t >> arrange(-t.col2))
+
+    @tables(['df3'])
+    def test_multiple(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t: t >> arrange(t.col2, -t.col3, -t.col4)
+        )
+
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t: t >> arrange(t.col2) >> arrange(-t.col3, -t.col4)
+        )
+
+
+class TestGroupBy:
+
+    @tables(['df3'])
+    def test_ungroup(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1, t.col2) >> ungroup()
+        )
+
+    @tables(['df3'])
+    def test_select(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1, t.col2) >> select(t.col1, t.col3)
+        )
+
+    @tables(['df3'])
+    def test_mutate(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> mutate(c1xc2 = t.col1 * t.col2) >> group_by(λ.c1xc2)
+        )
+
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1, t.col2) >> mutate(c1xc2 = t.col1 * t.col2)
+        )
+
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1, t.col2) >> mutate(col1 = t.col1 * t.col2)
+        )
+
+    @tables(['df1', 'df3'])
+    def test_grouped_join(self, df1_x, df1_y, df3_x, df3_y):
+        # Joining a grouped table should always throw an exception
+        assert_result_equal(
+            (df1_x, df3_x), (df1_y, df3_y),
+            lambda t, u: t >> group_by(λ.col1) >> join(u, t.col1 == u.col1, how = 'left'),
+            exception = ValueError
+        )
+
+        assert_result_equal(
+            (df1_x, df3_x), (df1_y, df3_y),
+            lambda t, u: t >> join(u >> group_by(λ.col1), t.col1 == u.col1, how = 'left'),
+            exception = ValueError
+        )
+
+    @tables(['df1', 'df3'])
+    @pytest.mark.parametrize('how', ['inner', 'left'])
+    def test_ungrouped_join(self, df1_x, df1_y, df3_x, df3_y, how):
+        # After ungrouping joining should work again
+        assert_result_equal(
+            (df1_x, df3_x), (df1_y, df3_y),
+            lambda t, u:
+            t >> group_by(t.col1) >> ungroup() >> join(u, t.col1 == u.col1, how=how) >> full_sort()
+        )
+
+    @tables(['df3'])
+    def test_filter(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1) >> filter(t.col2 >= 2)
+        )
+
+    @tables(['df3'])
+    def test_arrange(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1) >> arrange(t.col1, -t.col3)
+        )
+
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1) >> arrange(-t.col4)
+        )
 
 
 class TestSummarise:
@@ -144,3 +405,21 @@ class TestSummarise:
             lambda t:
             t >> group_by(t.col1, t.col2) >> summarise(mean3 = t.col3.mean()) >> summarise(mean_of_mean3 = λ.mean3.mean())
         )
+
+    @tables(['df3'])
+    def test_select(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1, t.col2) >> summarise(mean3 = t.col3.mean()) >> select(t.col1, λ.mean3, t.col2)
+        )
+
+    @tables(['df3'])
+    def test_mutate(self, df3_x, df3_y):
+        assert_result_equal(
+            df3_x, df3_y,
+            lambda t:
+            t >> group_by(t.col1, t.col2) >> summarise(mean3 = t.col3.mean()) >> mutate(x10 = λ.mean3 * 10)
+        )
+
+    # TODO: Implement more test cases or summarise verb
