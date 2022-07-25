85ca13addf4152c26d3525c19614cf44f4f78a3f
Nicolas Camenisch <garnele007@gmail.com>
2022-07-25 21:20:56 +0200

    Improve __init__.py imports

--- src/pydiverse/transform/__init__.py
+++ src/pydiverse/transform/__init__.py
@@ -1,5 +1,14 @@
 from pydiverse.transform.core import functions
+from pydiverse.transform.core.alignment import aligned, eval_aligned
 from pydiverse.transform.core.dispatchers import verb
 from pydiverse.transform.core.expressions.lambda_getter import λ
+from pydiverse.transform.core.table import Table
 
-__all__ = ["λ"]
+__all__ = [
+    "Table",
+    "aligned",
+    "eval_aligned",
+    "functions",
+    "verb",
+    "λ",
+]
