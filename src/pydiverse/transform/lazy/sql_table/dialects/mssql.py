42bb19015c7f91844bfca86d7552c265ab515c68
Nicolas Camenisch <garnele007@gmail.com>
2023-07-19 17:12:27 +0200

    mssql: Implement mean operator

--- src/pydiverse/transform/lazy/sql_table/dialects/mssql.py
+++ src/pydiverse/transform/lazy/sql_table/dialects/mssql.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import sqlalchemy as sa
 
+from pydiverse.transform.core import ops
 from pydiverse.transform.core.util import OrderingDescriptor
 from pydiverse.transform.lazy.sql_table.sql_table import SQLTableImpl
 
@@ -25,3 +26,10 @@ class MSSqlTableImpl(SQLTableImpl):
 
         order_by_expressions.append(col.asc() if ordering.asc else col.desc())
         return order_by_expressions
+
+
+with MSSqlTableImpl.op(ops.Mean()) as op:
+
+    @op.auto
+    def _mean(x):
+        return sa.func.AVG(sa.cast(x, sa.Float()))
