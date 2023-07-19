8b056bd1421d0c5907ca36ee445d8c326b6ebaad
Nicolas Camenisch <garnele007@gmail.com>
2023-07-19 17:04:33 +0200

    Implement NULLS FIRST/LAST for mssql

--- src/pydiverse/transform/lazy/sql_table/dialects/mssql.py
+++ src/pydiverse/transform/lazy/sql_table/dialects/mssql.py
@@ -1,7 +1,27 @@
 from __future__ import annotations
 
+import sqlalchemy as sa
+
+from pydiverse.transform.core.util import OrderingDescriptor
 from pydiverse.transform.lazy.sql_table.sql_table import SQLTableImpl
 
 
 class MSSqlTableImpl(SQLTableImpl):
     _dialect_name = "mssql"
+
+    def _order_col(
+        self, col: sa.SQLColumnExpression, ordering: OrderingDescriptor
+    ) -> list[sa.SQLColumnExpression]:
+        # MSSQL doesn't support nulls first / nulls last
+        order_by_expressions = []
+
+        # asc implies nulls first
+        if not ordering.nulls_first and ordering.asc:
+            order_by_expressions.append(sa.func.iif(col.is_(None), 1, 0))
+
+        # desc implies nulls last
+        if ordering.nulls_first and not ordering.asc:
+            order_by_expressions.append(sa.func.iif(col.is_(None), 0, 1))
+
+        order_by_expressions.append(col.asc() if ordering.asc else col.desc())
+        return order_by_expressions
