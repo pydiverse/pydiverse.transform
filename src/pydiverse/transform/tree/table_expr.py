281ffcbe3decad8729d0c11de3f739a11477dbee
Finn Rudolph <finn.rudolph@t-online.de>
2024-09-03 13:54:53 +0200

    allow col access on arbitrary table expressions

--- src/pydiverse/transform/tree/table_expr.py
+++ src/pydiverse/transform/tree/table_expr.py
@@ -1,4 +1,26 @@
 from __future__ import annotations
 
+from pydiverse.transform.tree import col_expr
 
-class TableExpr: ...
+
+class TableExpr:
+    name: str | None
+
+    def __getitem__(self, key: str) -> col_expr.Col:
+        if not isinstance(key, str):
+            raise TypeError(
+                f"argument to __getitem__ (bracket `[]` operator) on a Table must be a "
+                f"str, got {type(key)} instead."
+            )
+        return col_expr.Col(key, self)
+
+    def __getattr__(self, name: str) -> col_expr.Col:
+        return col_expr.Col(name, self)
+
+    def __eq__(self, rhs):
+        if not isinstance(rhs, TableExpr):
+            return False
+        return id(self) == id(rhs)
+
+    def __hash__(self):
+        return id(self)
