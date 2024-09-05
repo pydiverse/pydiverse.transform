2cb34252ba1edcce8be6fe8a6678368072eb199b
Finn Rudolph <finn.rudolph@t-online.de>
2024-09-05 12:01:55 +0200

    add special polars export for duckdb
    for whatever reason, the SQLAlchemy one does not work. Maybe we could also
    use the python duckdb library? (it at least as a `pl` module)

--- src/pydiverse/transform/backend/duckdb.py
+++ src/pydiverse/transform/backend/duckdb.py
@@ -1,7 +1,20 @@
 from __future__ import annotations
 
+import polars as pl
+
+from pydiverse.transform.backend import sql
 from pydiverse.transform.backend.sql import SqlImpl
+from pydiverse.transform.backend.targets import Polars, Target
+from pydiverse.transform.tree.table_expr import TableExpr
 
 
 class DuckDbImpl(SqlImpl):
     dialect_name = "duckdb"
+
+    @staticmethod
+    def export(expr: TableExpr, target: Target):
+        if isinstance(target, Polars):
+            engine = sql.get_engine(expr)
+            with engine.connect() as conn:
+                return pl.read_database(DuckDbImpl.build_query(expr), connection=conn)
+        return SqlImpl.export(expr, target)
