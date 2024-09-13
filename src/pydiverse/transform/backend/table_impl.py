d30034c7d57099a833578779b9242fabe1b5564d
Finn Rudolph <finn.rudolph@t-online.de>
2024-09-13 20:57:50 +0200

    clean up TableImpl

--- src/pydiverse/transform/backend/table_impl.py
+++ src/pydiverse/transform/backend/table_impl.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-import warnings
 from typing import TYPE_CHECKING, Any
 
 from pydiverse.transform import ops
 from pydiverse.transform.backend.targets import Target
-from pydiverse.transform.errors import FunctionTypeError
-from pydiverse.transform.ops import Ftype
 from pydiverse.transform.tree.col_expr import (
     Col,
     LiteralCol,
@@ -25,28 +22,6 @@ if TYPE_CHECKING:
 class TableImpl:
     """
     Base class from which all table backend implementations are derived from.
-    It tracks various metadata that is relevant for all backends.
-
-    Attributes:
-        name: The name of the table.
-
-        selects: Ordered set of selected names.
-        named_cols: Map from name to column uuid containing all columns that
-            have been named.
-        available_cols: Set of UUIDs that can be referenced in symbolic
-            expressions. This set gets used to validate verb inputs. It usually
-            contains the same uuids as the col_exprs. Only a summarising
-            operation resets this.
-        col_expr: Map from uuid to the `SymbolicExpression` that corresponds
-            to this column.
-        col_dtype: Map from uuid to the datatype of the corresponding column.
-            It is the responsibility of the backend to keep track of
-            this information.
-
-        grouped_by: Ordered set of columns by which the table is grouped by.
-        intrinsic_grouped_by: Ordered set of columns representing the underlying
-            grouping level of the table. This gets set when performing a
-            summarising operation.
     """
 
     registry = OperatorRegistry("TableImpl")
@@ -93,90 +68,9 @@ class TableImpl:
         """
         return repr(expr)
 
-    #### Verb Callbacks ####
-
-    def preverb_hook(self, verb: str, *args, **kwargs) -> None:
-        """Hook that gets called right after `copy` inside a verb
-
-        This gives the backend a chance to react and modify it's state. This
-        can, for example, be used to create a subquery based on specific
-        conditions.
-
-        :param verb: The name of the verb
-        :param args: The arguments passed to the verb
-        :param kwargs: The keyword arguments passed to the verb
-        """
-        ...
-
-    #### Symbolic Operators ####
-
     @classmethod
     def op(cls, operator: Operator, **kwargs) -> OperatorRegistrationContextManager:
         return OperatorRegistrationContextManager(cls.registry, operator, **kwargs)
-
-    #### Helpers ####
-
-    @classmethod
-    def _get_op_ftype(
-        cls, args, operator: Operator, override_ftype: Ftype = None, strict=False
-    ) -> Ftype:
-        """
-        Get the ftype based on a function implementation and the arguments.
-
-            e(e) -> e       a(e) -> a       w(e) -> w
-            e(a) -> a       a(a) -> Err     w(a) -> w
-            e(w) -> w       a(w) -> Err     w(w) -> Err
-
-        If the implementation ftype is incompatible with the arguments, this
-        function raises an Exception.
-        """
-
-        ftypes = [arg.ftype for arg in args]
-        op_ftype = override_ftype or operator.ftype
-
-        if op_ftype == Ftype.EWISE:
-            if Ftype.WINDOW in ftypes:
-                return Ftype.WINDOW
-            if Ftype.AGGREGATE in ftypes:
-                return Ftype.AGGREGATE
-            return op_ftype
-
-        if op_ftype == Ftype.AGGREGATE:
-            if Ftype.WINDOW in ftypes:
-                if strict:
-                    raise FunctionTypeError(
-                        "Can't nest a window function inside an aggregate function"
-                        f" ({operator.name})."
-                    )
-                else:
-                    # TODO: Replace with logger
-                    warnings.warn(
-                        "Nesting a window function inside an aggregate function is not"
-                        " supported by SQL backend."
-                    )
-            if Ftype.AGGREGATE in ftypes:
-                raise FunctionTypeError(
-                    "Can't nest an aggregate function inside an aggregate function"
-                    f" ({operator.name})."
-                )
-            return op_ftype
-
-        if op_ftype == Ftype.WINDOW:
-            if Ftype.WINDOW in ftypes:
-                if strict:
-                    raise FunctionTypeError(
-                        "Can't nest a window function inside a window function"
-                        f" ({operator.name})."
-                    )
-                else:
-                    warnings.warn(
-                        "Nesting a window function inside a window function is not"
-                        " supported by SQL backend."
-                    )
-            return op_ftype
-
-
-#### MARKER OPERATIONS #########################################################
 
 
 with TableImpl.op(ops.NullsFirst()) as op:
@@ -191,9 +85,6 @@ with TableImpl.op(ops.NullsLast()) as op:
     @op.auto
     def _nulls_last(_):
         raise RuntimeError("This is just a marker that never should get called")
-
-
-#### ARITHMETIC OPERATORS ######################################################
 
 
 with TableImpl.op(ops.Add()) as op:
@@ -323,9 +214,6 @@ with TableImpl.op(ops.Abs()) as op:
         return abs(x)
 
 
-#### BINARY OPERATORS ##########################################################
-
-
 with TableImpl.op(ops.And()) as op:
 
     @op.auto
@@ -373,9 +261,6 @@ with TableImpl.op(ops.Invert()) as op:
     @op.auto
     def _invert(x):
         return ~x
-
-
-#### COMPARISON OPERATORS ######################################################
 
 
 with TableImpl.op(ops.Equal()) as op:
