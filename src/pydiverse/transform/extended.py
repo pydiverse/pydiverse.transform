155c6ad3ee616827d6b0923dd56fadb1c206fbc8
Finn Rudolph <finn.rudolph@t-online.de>
2024-10-04 14:58:58 +0200

    put dense_rank in public interface

--- src/pydiverse/transform/extended.py
+++ src/pydiverse/transform/extended.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from .common import *  # noqa: F403
 from .common import __all__ as __common
-from .pipe.functions import count, lit, max, min, rank, row_number, when
+from .pipe.functions import count, dense_rank, lit, max, min, rank, row_number, when
 from .pipe.verbs import filter
 
 __all__ = __common + [
     "filter",
     "count",
+    "dense_rank",
     "max",
     "min",
     "rank",
