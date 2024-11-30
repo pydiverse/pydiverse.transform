32f3fec963c9872f244b5740d1910ff24eb2006f
Finn Rudolph <finn.rudolph@t-online.de>
2024-11-30 18:38:59 +0100

    add operator store and basic expression gen

--- fuzz.py
+++ fuzz.py
@@ -8,19 +8,26 @@ import numpy as np
 import polars as pl
 
 import pydiverse.transform as pdt
-from pydiverse.transform._internal.backend.polars import polars_type
+from pydiverse.transform._internal.ops import ops
+from pydiverse.transform._internal.ops.op import Operator
+from pydiverse.transform._internal.ops.signature import Signature
+from pydiverse.transform._internal.tree.col_expr import ColFn
+from pydiverse.transform._internal.tree.types import Tvar
 
 rng = np.random.default_rng()
 letters = list(string.printable)
+
+ALL_TYPES = [pdt.Int(), pdt.Float(), pdt.Bool(), pdt.String()]
+MEAN_HEIGHT = 4
 
 
 def gen_table(rows: int, types: dict[pdt.Dtype, int]) -> pl.DataFrame:
     d = pl.DataFrame()
 
     rng_fns = {
-        pdt.Float64(): rng.standard_normal,
-        pdt.Int64(): partial(rng.integers, -(1 << 13), 1 << 13),
-        pdt.Bool(): partial(rng.integers, 0, 1),
+        pdt.Float(): rng.standard_normal,
+        pdt.Int(): partial(rng.integers, -(1 << 13), 1 << 13),
+        pdt.Bool(): partial(rng.integers, 0, 1, dtype=bool),
         pdt.String(): (
             lambda rows: np.array(
                 [
@@ -31,15 +38,63 @@ def gen_table(rows: int, types: dict[pdt.Dtype, int]) -> pl.DataFrame:
         ),
     }
 
-    for t, fn in rng_fns.items():
-        if t in types:
+    for ty, fn in rng_fns.items():
+        if ty in types:
             d = d.with_columns(
                 **{
-                    f"{t.__class__.__name__.lower()} #{i+1}": pl.lit(fn(rows)).cast(
-                        polars_type(t)
-                    )
-                    for i in range(types[t])
+                    f"{ty.__class__.__name__.lower()} #{i+1}": pl.lit(fn(rows))
+                    for i in range(types[ty])
                 }
             )
 
     return d
+
+
+ops_with_return_type: dict[pdt.Dtype, list[tuple[Operator, Signature]]] = {
+    ty: [] for ty in ALL_TYPES
+}
+
+for op in ops.__dict__.values():
+    if not isinstance(op, Operator):
+        continue
+    for sig in op.signatures:
+        if isinstance(sig.return_type, Tvar):
+            for ty in ALL_TYPES:
+                ops_with_return_type[ty].append(
+                    (
+                        op,
+                        Signature(
+                            *(
+                                ty if isinstance(param, Tvar) else param
+                                for param in sig.types
+                            ),
+                            return_type=ty,
+                        ),
+                    )
+                )
+        else:
+            ops_with_return_type[sig.return_type].append((op, sig))
+
+
+def gen_expr(
+    dtype: pdt.Dtype, cols: dict[pdt.Dtype, list[str]], q: float = 0.0
+) -> pdt.ColExpr:
+    if q > 1:
+        return rng.choice(cols[dtype])
+
+    op, sig = rng.choice(ops_with_return_type[dtype])
+    assert isinstance(op, Operator)
+    assert isinstance(sig, Signature)
+
+    args = []
+    for param in sig.types[: len(sig.types) - sig.is_vararg]:
+        args.append(gen_expr(param, cols, q + rng.exponential(1 / MEAN_HEIGHT)))
+
+    if sig.is_vararg:
+        nargs = int(rng.chisquare(4))
+        for _ in range(nargs):
+            args.append(
+                gen_expr(sig.types[-1], cols, q + rng.exponential(1 / MEAN_HEIGHT))
+            )
+
+    return ColFn(op, *args)
